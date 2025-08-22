5be2e67a457bf5834cceb3dba84563599fdb5ad7
gabriel.hochscheidt <gabriel.hochscheidt@megaembalagens.com.br>
2025-08-22 17:12:09 -0300

    !notworking

--- src/app/pages/avaliacao/avaliacao.component.ts
+++ src/app/pages/avaliacao/avaliacao.component.ts
@@ -3,12 +3,20 @@ import { Component } from '@angular/core';
 import { DxDataGridModule, DxToolbarModule, DxButtonModule, DxDrawerModule, DxTemplateModule } from 'devextreme-angular';
 import { SetorInterface } from '../../models/interfaces/setores.interface';
 import { SensoInterface } from '../../models/interfaces/senso.interface';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MenuToolbarService } from '../../services';
 
 
 @Component({
   selector: 'app-avaliacao',
   standalone: true,
-  imports: [DxDataGridModule, DxToolbarModule, DxButtonModule, DxDrawerModule, DxTemplateModule],
+  imports: [DxDrawerModule,
+  DxButtonModule,
+  DxTemplateModule,
+  FormsModule,
+  CommonModule,
+  DxDataGridModule],
   templateUrl: './avaliacao.component.html',
   styleUrl: './avaliacao.component.css'
 })
@@ -17,12 +25,46 @@ export class AvaliacaoComponent {
   setoresList: SetorInterface[] = [];
   agrupadoresList: SensoInterface[] = [];
 
-  drawerAberto = false;
+  isNovaPlanilha = false;
+  setorEditando?: SetorInterface;
 
-  openPlanilhas(){
-    this.drawerAberto = true;
-    console.log('click')
+  drawerNovo: boolean = false;
+
+  constructor(
+    public menuService : MenuToolbarService
+  ){}
+
+  openPlanilhas(evt : any){
+    this.drawerNovo = true;
+    this.setorEditando = {
+      id: 1,
+      nome: '',
+      descricao: '',
+      ativo: true,
+      itens: [],
+    };
+    console.log('Botão clicado, abrindo novo drawer.');
   }
+
+  fecharNovoDrawer() {
+    this.drawerNovo = false;
+  }
+
+  // ... outros métodos
+
+
+  // openDrawer() {
+  //   this.setorEditando = {
+  //     id: 1,
+  //     nome: '',
+  //     descricao: '',
+  //     ativo: true,
+  //     itens: [],
+  //   };
+  //   this.isNovaPlanilha = true;
+  //   this.drawerAberto = true;
+  //   console.log('click')
+  // }
 
   changeState(){
 
